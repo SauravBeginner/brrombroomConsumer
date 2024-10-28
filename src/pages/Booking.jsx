783a5f3a2b7e6760ead71ae0acb4b68af59ece2b
SauravBeginner <kundusaurav1996@gmail.com>
2024-10-29 01:28:20 +0530

    accept booking fixed

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import bookingService from "../appwrite/booking"; // Import the BookingService
-import authService from "../appwrite/auth";
 import { useAuth } from "../context/AuthContext";
 
 const Booking = () => {
